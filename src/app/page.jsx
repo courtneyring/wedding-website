33b1aa81ca59ff03c307564902eb834d2210c49b
Courtney <courtneyring3@gmail.com>
2025-11-05 16:14:35 -0500

    more landing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { OverlayHero, Banner } from "@courtneyring/components-library";
-import content from "../assets/json/home.json";
+import { OverlayHero, Banner, ImageTextBlock, Container, BorderedBackground } from "@courtneyring/components-library";
+import content from "../copy/home";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <OverlayHero {...content.hero} />
+
+        <Container color="color-white" maxWidth="lg">
+            <ImageTextBlock {...content.imageTextVenue} reverse />
+        </Container>
+        <Container color="color-neutral-light" maxWidth="lg">
+          <ImageTextBlock {...content.imageTextPlanning} />
+        </Container>
         <Banner {...content.banner} />
       </main>
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
