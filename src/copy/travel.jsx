1042d65d6737320902ffb53c9012fa1f65d8f69b
Courtney Ring <courtneyring3@gmail.com>
2026-05-26 14:16:22 -0400

    small copy update

--- src/copy/travel.jsx
+++ src/copy/travel.jsx
@@ -7,7 +7,7 @@ export default {
     pos: "center 20%",
   },
   banner: {
-    body: "This page has all the essential information for getting to Rhinebeck and getting around during the weekend. We’ve included details for airports, driving, and local transportation to make planning your trip easier.",
+    body: "This page has all the essential information for getting to Rhinebeck. We’ve included details for airports, driving, and trains to make planning your trip easier.",
   },
   air: {
     header: "By Air",
