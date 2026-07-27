b4253044f230de659bc0d4b6d1d18a7404fad63f
Courtney Ring <courtneyring3@gmail.com>
2026-07-27 13:19:05 -0400

    fix icons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@100"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,0&display=block"
           rel="stylesheet"
         />
         <meta
